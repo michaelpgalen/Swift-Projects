2080e6de173490df07b526ba8d11c112853704ec
Michael Galen <52191377+michaelpgalen@users.noreply.github.com>
2022-01-28 16:11:09 -0800

    Adds WeSplit app bundle

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michael Galen on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
