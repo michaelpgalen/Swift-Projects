b0e8d30099f05ba03763bc4d8faa7df87164fad8
Michael Galen <52191377+michaelpgalen@users.noreply.github.com>
2022-01-28 16:10:39 -0800

    Adds WordScramble app bundle

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Michael Galen on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
